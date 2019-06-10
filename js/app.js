3bbb79a6322b5c7b3eef19ba6e657f9af60d8011
m-zaremba <zaremba_michal@wp.pl>
2019-06-10 19:09:02 +0200

    jQuery simple tabs and contact form code initial commit

--- js/app.js
+++ js/app.js
@@ -76,6 +76,21 @@ $(function() {
     }
   })
 
+/* --------------- SIMPLE TABS --------------- */
+
+  const tabs = $('.tabs').find('li');
+  const text = $('.tabs').find('div');
+
+  tabs.each(function(i) {
+    $(this).on('click', function() {
+      if (text.eq(i).css('display') === 'none') {
+        text.eq(i).slideDown();
+      } else {
+        text.eq(i).slideUp();
+      }
+    })
+  })
+
 /* --------------- PARALLAX --------------- */
 
 function parallaxEffect() {
@@ -126,6 +141,64 @@ function parallaxEffect() {
   }
 
   parallaxEffect();
+
+/* --------------- CONTACT FORM --------------- */
+
+function formHandler() {
+
+    var form = $('#contactForm');
+    var name = $('#nameInput');
+    var email = $('#emailInput');
+    var message = $('#messageInput');
+    var error = $('.error');
+
+    form.on('submit', function(e) {
+
+      var nameInputValue = name.val();
+      var emailInputValue = email.val();
+      var messageInputValue = message.val();
+
+      var errorArr = [];
+
+      function validateEmail(emailInputValue) {
+        var emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+        return emailPattern.test(email.val());
+      }
+
+      if (validateEmail() === false) {
+        //e.preventDefault();
+        errorArr.push('Email musi posiadać znak @ oraz .');
+        email.addClass('marked');
+      } else {
+        email.removeClass('marked');
+      }
+
+      if (nameInputValue.length <= 5) {
+        //e.preventDefault();
+        errorArr.push('Twoje imię musi być dłuższe niż 5 liter.');
+        name.addClass('marked');
+      } else {
+        name.removeClass('marked');
+      }
+
+      if (messageInputValue.length <= 10) {
+        //e.preventDefault();
+        errorArr.push('Wiadomośc musi mieć więcej niż 10 znaków.');
+        message.addClass('marked');
+      } else {
+        message.removeClass('marked');
+      }
+
+      if (errorArr.length > 0) {
+        e.preventDefault();
+        error.text(errorArr.join('\n'));
+      } else {
+        error.text('Nie popędzaj - robim co możem...');
+      }
+    })
+  }
+
+  formHandler();
 
 
 });
